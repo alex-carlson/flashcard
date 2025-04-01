3af8fc159c53babba8509a413f70de146da5d540
Alex Carlson <alex@acwd.me>
2025-04-01 16:29:03 -0400

    homepage updates

--- src/routes/Home/+page.svelte
+++ src/routes/Home/+page.svelte
@@ -1,4 +1,16 @@
 <script>
     import Search from "../../lib/Search.svelte";
+    let token = localStorage.getItem("token");
+    let isLoggedIn = !!token;
 </script>
-<Search />
+
+<div class="container">
+    <h1>Pick a category</h1>
+    <Search />
+    <!-- if logged in -->
+    {#if isLoggedIn}
+        <h1>Or <a href="#/upload">create a new one</a>!</h1>
+    {:else}
+        <h1>Or <a href="#/login">log in</a> to create one!</h1>
+    {/if}
+</div>
