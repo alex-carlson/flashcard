85116482736da12b93c64ec93c9d5636b051bade
Alex Carlson <alex-carlson@users.noreply.github.com>
2025-06-13 12:48:58 -0400

    updated vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: vercel(),
+		adapter: adapter({
+
+		}),
 	}
 };
 
