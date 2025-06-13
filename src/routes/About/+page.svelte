74b58f84e70e136d91e103fb08276f7bc6ed5af0
Alex Carlson <alex-carlson@users.noreply.github.com>
2025-06-13 12:45:33 -0400

    updated about page

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,30 +1,31 @@
-<!-- template an about page -->
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		document.title = 'About Quizzems';
+	});
+</script>
+
 <div class="about">
-    <div class="container white padding align-left">
-        <h1>About Quizzems</h1>
-        <p>
-            Quizzems is a trivia platform designed to help study for bar trivia.
-            With the design pillars to load quickly so that if you are on the
-            train or otherwise lose connection, you don't have to wait to get
-            back online, and also to be free of intrusive ads and bloat.
-        </p>
-        <p>
-            It's also easy for anyone to create their own quizzes, I strive to
-            make the process as streamlined as possible so you can quickly and
-            easily set up a new quiz from wherever you are, and edit at your
-            leisure. Save drafts, edit images and answers, keep your quiz
-            private until you're ready to share it!
-        </p>
-        <p>
-            Quizzems is created and maintained by <a
-                href="https://acwd.me/"
-                target="_blank">Alex Carlson</a
-            >
-            for free. As the community grows, so do server costs, so if you enjoy
-            Quizzems, consider
-            <a href="https://www.paypal.biz/acwd" target="_blank"
-                >beering me a beer!</a
-            >!
-        </p>
-    </div>
+	<div class="container white padding align-left">
+		<h1>About Quizzems</h1>
+		<p>
+			Quizzems is a trivia platform designed to help study for bar trivia. With the design pillars
+			to load quickly so that if you are on the train or otherwise lose connection, you don't have
+			to wait to get back online, and also to be free of intrusive ads and bloat.
+		</p>
+		<p>
+			It's also easy for anyone to create their own quizzes, I strive to make the process as
+			streamlined as possible so you can quickly and easily set up a new quiz from wherever you are,
+			and edit at your leisure. Save drafts, edit images and answers, keep your quiz private until
+			you're ready to share it!
+		</p>
+		<p>
+			Quizzems is created and maintained by <a href="https://acwd.me/" target="_blank"
+				>Alex Carlson</a
+			>
+			for free. As the community grows, so do server costs, so if you enjoy Quizzems, consider
+			<a href="https://www.paypal.biz/acwd" target="_blank">beering me a beer!</a>!
+		</p>
+	</div>
 </div>
