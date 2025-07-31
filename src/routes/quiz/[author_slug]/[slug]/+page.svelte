aafe98e87b193ffea93c44a0356ea46b0b905469
Alex Carlson <alex-carlson@users.noreply.github.com>
2025-07-31 10:58:05 -0400

    attempts to improve metadata for sharing quiz links

--- src/routes/quiz/[author_slug]/[slug]/+page.svelte
+++ src/routes/quiz/[author_slug]/[slug]/+page.svelte
@@ -4,10 +4,12 @@
 	import { onDestroy, onMount } from 'svelte';
 	import { fetchCollectionByAuthorAndSlug } from '$lib/api/collections';
 	import { fetchUserBySlug } from '$lib/api/user';
+	import { createQuizStore } from '$stores/quiz';
 	let author_slug = null;
 	let author_id = null; // Keeping this for reference, but using author_slug in the page params
 	let category = null;
 	let collectionId = null;
+	const quiz = createQuizStore();
 
 	const unsubscribe = page.subscribe(($page) => {
 		author_slug = $page.params.author_slug;
@@ -30,6 +32,20 @@
 
 	onDestroy(unsubscribe);
 </script>
+
+<svelte:head>
+	{#if category && author_slug}
+		<title>Try {category} by {author_slug}!</title>
+		<meta property="og:title" content="Try {category} by {author_slug}!" />
+		<meta name="twitter:title" content="Try {category} by {author_slug}!" />
+		<!-- Social image tags -->
+		{#if $quiz?.thumbnail}
+			<meta property="og:image" content={$quiz.thumbnail} />
+			<meta name="twitter:image" content={$quiz.thumbnail} />
+		{/if}
+		<meta name="twitter:card" content="summary_large_image" />
+	{/if}
+</svelte:head>
 
 <div>
 	<FlashCards {collectionId} />
