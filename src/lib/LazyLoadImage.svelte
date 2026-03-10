802e90130e194134dd68aed5bfc37816ee4b9939
Alex Carlson <alex-carlson@users.noreply.github.com>
2026-03-10 09:51:59 -0400

    remove blur on placeholder

--- src/lib/LazyLoadImage.svelte
+++ src/lib/LazyLoadImage.svelte
@@ -132,19 +132,3 @@
 		{/if}
 	{/if}
 </div>
-
-<style>
-	/* Blurred low-res placeholder */
-	.placeholder-blur {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-		filter: blur(8px);
-		transform: scale(1.05);
-		z-index: 1;
-		opacity: 0.8;
-	}
-</style>
